32826be8944e60fd760eeaeedd8349abe31d13ab
closimo-prog <teams.closimo@gmail.com>
2026-03-18 11:48:31 +0500

    Delete delete_lines.py

--- delete_lines.py
+++
@@ -1,9 +0,0 @@
-with open('index.html', 'r', encoding='utf-8') as f:
-    lines = f.readlines()
-
-# Delete lines 985 to 1064 (0-indexed 984 to 1064)
-del lines[984:1064]
-
-with open('index.html', 'w', encoding='utf-8') as f:
-    f.writelines(lines)
-print('Deleted broken script block')
